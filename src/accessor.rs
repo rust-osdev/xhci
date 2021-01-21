a66a281ec72175b13aedc211a2db339b6e4bac90
toku-sa-n <tokusan441@gmail.com>
2021-01-21 21:16:43 +0900

    chore: mark `Accessor` as public
    To return this from methods.

--- src/accessor.rs
+++ src/accessor.rs
@@ -3,6 +3,7 @@ use core::{convert::TryInto, marker::PhantomData, mem, ptr};
 use os_units::Bytes;
 use x86_64::{PhysAddr, VirtAddr};
 
+/// An accessor to a memory region.
 pub(crate) struct Accessor<T, M>
 where
     T: ?Sized,
@@ -17,6 +18,29 @@ impl<T, M> Accessor<T, M>
 where
     M: Mapper,
 {
+    /// Reads a value from memory.
+    pub fn read(&self) -> T {
+        // SAFETY: `Accessor::new` ensures that the all necessary conditions are fulfilled.
+        unsafe { ptr::read_volatile(self.virt.as_ptr()) }
+    }
+
+    /// Writes `value` to memory.
+    pub fn write(&mut self, value: T) {
+        // SAFETY: `Accessor::new` ensures that the all necessary conditions are fulfilled.
+        unsafe { ptr::write_volatile(self.virt.as_mut_ptr(), value) }
+    }
+
+    /// Update a value on memory by reading a value from the memory, modifying the value, and writing the
+    /// modified value.
+    pub fn update<U>(&mut self, f: U)
+    where
+        U: FnOnce(&mut T),
+    {
+        let mut v = self.read();
+        f(&mut v);
+        self.write(v);
+    }
+
     /// # Safety
     ///
     /// Caller must ensure that:
@@ -41,30 +65,56 @@ where
             _marker: PhantomData,
         }
     }
-
-    pub(crate) fn read(&self) -> T {
-        // SAFETY: `Accessor::new` ensures that the all necessary conditions are fulfilled.
-        unsafe { ptr::read_volatile(self.virt.as_ptr()) }
-    }
-
-    pub(crate) fn write(&mut self, v: T) {
-        // SAFETY: `Accessor::new` ensures that the all necessary conditions are fulfilled.
-        unsafe { ptr::write_volatile(self.virt.as_mut_ptr(), v) }
-    }
-
-    pub(crate) fn update<U>(&mut self, f: U)
-    where
-        U: FnOnce(&mut T),
-    {
-        let mut v = self.read();
-        f(&mut v);
-        self.write(v);
-    }
 }
 impl<T, M> Accessor<[T], M>
 where
     M: Mapper,
 {
+    /// Reads the `index`th value from memory.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `index > self.len()`.
+    pub fn read(&self, index: usize) -> T {
+        assert!(
+            index < self.len(),
+            "index out of bounds: the length is {} but the index is {}",
+            self.len(),
+            index
+        );
+
+        // SAFETY: `Accessor::new_slice` ensures that the all necessary conditions are fulfilled.
+        unsafe { ptr::read_volatile(self.addr(index).as_ptr()) }
+    }
+
+    /// Writes `value` on memory as the `index`th element.
+    pub fn write(&self, index: usize, value: T) {
+        assert!(
+            index < self.len(),
+            "index out of bounds: the length is {} but the index is {}",
+            self.len(),
+            index
+        );
+
+        // SAFETY: `Accessor::new_slice` ensures that the all necessary conditions are fulfilled.
+        unsafe { ptr::write_volatile(self.addr(index).as_mut_ptr(), value) }
+    }
+
+    /// Update the `index`th value by reading the value, modifying with `f`, and writing it on memory.
+    pub fn update<U>(&self, i: usize, f: U)
+    where
+        U: FnOnce(&mut T),
+    {
+        let mut v = self.read(i);
+        f(&mut v);
+        self.write(i, v);
+    }
+
+    /// Returns the number of elements this accessor can access.
+    pub fn len(&self) -> usize {
+        self.bytes.as_usize() / mem::size_of::<T>()
+    }
+
     /// # Safety
     ///
     /// Caller must ensure that:
@@ -95,45 +145,8 @@ where
         }
     }
 
-    pub(crate) fn read(&self, i: usize) -> T {
-        assert!(
-            i < self.len(),
-            "index out of bounds: the length is {} but the index is {}",
-            self.len(),
-            i
-        );
-
-        // SAFETY: `Accessor::new_slice` ensures that the all necessary conditions are fulfilled.
-        unsafe { ptr::read_volatile(self.addr(i).as_ptr()) }
-    }
-
-    pub(crate) fn write(&self, i: usize, v: T) {
-        assert!(
-            i < self.len(),
-            "index out of bounds: the length is {} but the index is {}",
-            self.len(),
-            i
-        );
-
-        // SAFETY: `Accessor::new_slice` ensures that the all necessary conditions are fulfilled.
-        unsafe { ptr::write_volatile(self.addr(i).as_mut_ptr(), v) }
-    }
-
-    pub(crate) fn update<U>(&self, i: usize, f: U)
-    where
-        U: FnOnce(&mut T),
-    {
-        let mut v = self.read(i);
-        f(&mut v);
-        self.write(i, v);
-    }
-
     fn addr(&self, i: usize) -> VirtAddr {
         self.virt + mem::size_of::<T>() * i
-    }
-
-    fn len(&self) -> usize {
-        self.bytes.as_usize() / mem::size_of::<T>()
     }
 }
 impl<T, M> Drop for Accessor<T, M>

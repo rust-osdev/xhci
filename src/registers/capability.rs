1fa3afa950af74ba67831554138b092a8a1626ae
toku-sa-n <tokusan441@gmail.com>
2021-01-22 12:54:53 +0900

    chore: add `must_use` attributes

--- src/registers/capability.rs
+++ src/registers/capability.rs
@@ -9,6 +9,7 @@ use core::convert::TryInto;
 pub struct CapabilityRegistersLength(u8);
 impl CapabilityRegistersLength {
     /// Returns the length of the Capability Registers.
+    #[must_use]
     pub fn get(&self) -> u8 {
         self.0
     }
@@ -19,11 +20,13 @@ impl CapabilityRegistersLength {
 pub struct StructuralParameters1(u32);
 impl StructuralParameters1 {
     /// Returns the number of available device slots.
+    #[must_use]
     pub fn number_of_device_slots(&self) -> u8 {
         self.0.get_bits(0..=7).try_into().unwrap()
     }
 
     /// Returns the number of ports.
+    #[must_use]
     pub fn number_of_ports(&self) -> u8 {
         self.0.get_bits(24..=31).try_into().unwrap()
     }
@@ -37,11 +40,13 @@ impl StructuralParameters2 {
     ///
     /// Note that the `ERST Max` field of the Structural Parameters 2 register contains the exponential
     /// value, but this method returns the calculated value.
+    #[must_use]
     pub fn event_ring_segment_table_max(&self) -> u16 {
         2_u16.pow(self.erst_max())
     }
 
     /// Returns the number of scratchpads that xHC needs.
+    #[must_use]
     pub fn max_scratchpad_buffers(&self) -> u32 {
         let h = self.max_scratchpad_buffers_hi();
         let l = self.max_scratchpad_buffers_lo();
@@ -69,6 +74,7 @@ pub struct CapabilityParameters1(u32);
 impl CapabilityParameters1 {
     /// Returns `true` if the xHC uses 64 byte Context data structures, and `false` if the xHC uses
     /// 32 byte Context data structures.
+    #[must_use]
     pub fn context_size(&self) -> bool {
         self.0.get_bit(2)
     }
@@ -76,6 +82,7 @@ impl CapabilityParameters1 {
     /// Returns the offset of the xHCI extended capability list from the MMIO base. If this value is
     /// zero, the list does not exist.
     /// The base address can be calculated by `(MMIO base) + (xECP) << 2`
+    #[must_use]
     pub fn xhci_extended_capabilities_pointer(&self) -> u16 {
         self.0.get_bits(16..=31).try_into().unwrap()
     }
@@ -86,6 +93,7 @@ impl CapabilityParameters1 {
 pub struct DoorbellOffset(u32);
 impl DoorbellOffset {
     /// Returns the offset of the Doorbell Array from the MMIO base.
+    #[must_use]
     pub fn get(&self) -> u32 {
         self.0
     }

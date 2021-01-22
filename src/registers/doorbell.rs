3aa646c63a6d8e6a926981444a1a6fcd3953253f
toku-sa-n <tokusan441@gmail.com>
2021-01-22 12:39:24 +0900

    chore: add `doorbell.rs`

---
+++ src/registers/doorbell.rs
@@ -0,0 +1,19 @@
+//! Doorbell Register.
+
+use bit_field::BitField;
+use core::convert::TryInto;
+
+/// The element of the Doorbell Array.
+#[repr(transparent)]
+pub struct Register(u32);
+impl Register {
+    /// Get a doorbell target.
+    pub fn doorbell_target(&self) -> u8 {
+        self.0.get_bits(0..=7).try_into().unwrap()
+    }
+
+    /// Set a doorbell target.
+    pub fn set_doorbell_target(&mut self, target: u8) {
+        self.0.set_bits(0..=7, target.into());
+    }
+}

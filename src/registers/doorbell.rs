404295c35c7717b7f648c78903d7e06a8f30b4ce
toku-sa-n <tokusan441@gmail.com>
2021-01-22 12:52:36 +0900

    chore: add `must_use` attribute

--- src/registers/doorbell.rs
+++ src/registers/doorbell.rs
@@ -8,6 +8,7 @@ use core::convert::TryInto;
 pub struct Register(u32);
 impl Register {
     /// Get a doorbell target.
+    #[must_use]
     pub fn doorbell_target(&self) -> u8 {
         self.0.get_bits(0..=7).try_into().unwrap()
     }

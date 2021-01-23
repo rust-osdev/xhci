f589db2352ed4aeccc3900319bcdb50c263b2e76
toku-sa-n <tokusan441@gmail.com>
2021-01-23 19:02:33 +0900

    chore: pass by value

--- src/registers/doorbell.rs
+++ src/registers/doorbell.rs
@@ -10,7 +10,7 @@ pub struct Register(u32);
 impl Register {
     /// Get a doorbell target.
     #[must_use]
-    pub fn doorbell_target(&self) -> u8 {
+    pub fn doorbell_target(self) -> u8 {
         self.0.get_bits(0..=7).try_into().unwrap()
     }
 

633bf995e4671fb000d0fd50f1aa6b9c97370d99
toku-sa-n <tokusan441@gmail.com>
2021-01-23 19:02:05 +0900

    chore: pass by value

--- src/registers/capability.rs
+++ src/registers/capability.rs
@@ -11,7 +11,7 @@ pub struct CapabilityRegistersLength(u8);
 impl CapabilityRegistersLength {
     /// Returns the length of the Capability Registers.
     #[must_use]
-    pub fn get(&self) -> u8 {
+    pub fn get(self) -> u8 {
         self.0
     }
 }
@@ -23,13 +23,13 @@ pub struct StructuralParameters1(u32);
 impl StructuralParameters1 {
     /// Returns the number of available device slots.
     #[must_use]
-    pub fn number_of_device_slots(&self) -> u8 {
+    pub fn number_of_device_slots(self) -> u8 {
         self.0.get_bits(0..=7).try_into().unwrap()
     }
 
     /// Returns the number of ports.
     #[must_use]
-    pub fn number_of_ports(&self) -> u8 {
+    pub fn number_of_ports(self) -> u8 {
         self.0.get_bits(24..=31).try_into().unwrap()
     }
 }
@@ -52,28 +52,28 @@ impl StructuralParameters2 {
     /// Note that the `ERST Max` field of the Structural Parameters 2 register contains the exponential
     /// value, but this method returns the calculated value.
     #[must_use]
-    pub fn event_ring_segment_table_max(&self) -> u16 {
+    pub fn event_ring_segment_table_max(self) -> u16 {
         2_u16.pow(self.erst_max())
     }
 
     /// Returns the number of scratchpads that xHC needs.
     #[must_use]
-    pub fn max_scratchpad_buffers(&self) -> u32 {
+    pub fn max_scratchpad_buffers(self) -> u32 {
         let h = self.max_scratchpad_buffers_hi();
         let l = self.max_scratchpad_buffers_lo();
 
         h << 5 | l
     }
 
-    fn erst_max(&self) -> u32 {
+    fn erst_max(self) -> u32 {
         self.0.get_bits(4..=7)
     }
 
-    fn max_scratchpad_buffers_hi(&self) -> u32 {
+    fn max_scratchpad_buffers_hi(self) -> u32 {
         self.0.get_bits(20..=25)
     }
 
-    fn max_scratchpad_buffers_lo(&self) -> u32 {
+    fn max_scratchpad_buffers_lo(self) -> u32 {
         self.0.get_bits(27..=31)
     }
 }
@@ -98,7 +98,7 @@ impl CapabilityParameters1 {
     /// Returns `true` if the xHC uses 64 byte Context data structures, and `false` if the xHC uses
     /// 32 byte Context data structures.
     #[must_use]
-    pub fn context_size(&self) -> bool {
+    pub fn context_size(self) -> bool {
         self.0.get_bit(2)
     }
 
@@ -106,7 +106,7 @@ impl CapabilityParameters1 {
     /// zero, the list does not exist.
     /// The base address can be calculated by `(MMIO base) + (xECP) << 2`
     #[must_use]
-    pub fn xhci_extended_capabilities_pointer(&self) -> u16 {
+    pub fn xhci_extended_capabilities_pointer(self) -> u16 {
         self.0.get_bits(16..=31).try_into().unwrap()
     }
 }
@@ -126,7 +126,7 @@ pub struct DoorbellOffset(u32);
 impl DoorbellOffset {
     /// Returns the offset of the Doorbell Array from the MMIO base.
     #[must_use]
-    pub fn get(&self) -> u32 {
+    pub fn get(self) -> u32 {
         self.0
     }
 }
@@ -138,7 +138,7 @@ pub struct RuntimeRegisterSpaceOffset(u32);
 impl RuntimeRegisterSpaceOffset {
     /// Returns the offset of the Runtime Registers from the MMIO base.
     #[must_use]
-    pub fn get(&self) -> u32 {
+    pub fn get(self) -> u32 {
         self.0
     }
 }

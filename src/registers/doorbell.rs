6ab414e0a0eb8cb7d93561e2df92ebf49e99c412
toku-sa-n <tokusan441@gmail.com>
2021-01-25 14:36:10 +0900

    chore: add `doorbell::Register::new`

--- src/registers/doorbell.rs
+++ src/registers/doorbell.rs
@@ -1,12 +1,42 @@
 //! Doorbell Register
 
+use super::capability::Capability;
+use accessor::Mapper;
 use bit_field::BitField;
+use core::convert::TryFrom;
 use core::{convert::TryInto, fmt};
 
 /// The element of the Doorbell Array.
 #[repr(transparent)]
+#[derive(Copy, Clone)]
 pub struct Register(u32);
 impl Register {
+    /// Creates a new accessor to the Doorbell Array.
+    ///
+    /// # Safety
+    ///
+    /// Caller must ensure that the only one accessor is created, otherwise it may cause undefined
+    /// behavior such as data race.
+    ///
+    /// # Errors
+    ///
+    /// This method may return a [`accessor::Error::NotAligned`] error if the start of the Doorbell
+    /// Array is not aligned.
+    pub unsafe fn new<M>(
+        mmio_base: usize,
+        capability: &Capability,
+        mapper: M,
+    ) -> Result<accessor::Array<Self, M>, accessor::Error>
+    where
+        M: Mapper,
+    {
+        let base = mmio_base + usize::try_from(capability.dboff.get()).unwrap();
+        accessor::Array::new(
+            base,
+            capability.hcsparams1.number_of_device_slots().into(),
+            mapper,
+        )
+    }
     /// Get a doorbell target.
     #[must_use]
     pub fn doorbell_target(&self) -> u8 {

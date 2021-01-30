b3807f51bcfdc8b6a53e20c42b2b43ce2d4eaa30
toku-sa-n <tokusan441@gmail.com>
2021-01-30 17:30:42 +0900

    chore: split `Slot` into `Byte32` and `Byte64`

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -1,7 +1,6 @@
 //! The xHC Contexts.
 
 pub use endpoint::Endpoint;
-pub use slot::Slot;
 
 pub mod endpoint;
 pub mod slot;
--- src/context/slot.rs
+++ src/context/slot.rs
@@ -1,55 +1,50 @@
 //! Slot Context
 
 use bit_field::BitField;
+use paste::paste;
 
-/// Slot Context.
-#[repr(transparent)]
-#[derive(Copy, Clone, Debug, Default, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct Slot([u32; 8]);
-impl Slot {
-    /// Creates a null Slot Context.
-    pub const fn new() -> Self {
-        Self([0; 8])
-    }
+macro_rules! cx {
+    ($bytes:expr,$len:expr) => {
+        paste! {
+            #[doc = $bytes " byte version of the Slot Context."]
+            #[repr(transparent)]
+            #[derive(Copy, Clone, Debug, Default, Ord, PartialOrd, Eq, PartialEq, Hash)]
+            pub struct [<Byte $bytes>]([u32;$len]);
+            impl [<Byte $bytes>]{
+                /// Creates a null Slot Context.
+                pub const fn new()->Self{
+                    Self([0;$len])
+                }
 
-    /// Converts [`Slot`] into an array.
-    ///
-    /// Use this method if the Context Size bit of the HCCPARAMS1 register is 0.
-    pub fn into_32byte(self) -> [u32; 8] {
-        self.0
-    }
+                #[doc = "Converts [`Byte" $bytes "`] to an array."]
+                pub fn [<into_ $bytes byte>](self)->[u32;$len]{
+                    self.0
+                }
 
-    /// Converts [`Slot`] into an array.
-    ///
-    /// Use this method if the Context Size bit of the HCCPARAMS1 register is 1.
-    pub fn into_64byte(self) -> [u32; 16] {
-        [
-            self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5], self.0[6], self.0[7],
-            0, 0, 0, 0, 0, 0, 0, 0,
-        ]
-    }
-
-    /// Sets the value of the Context Entries field.
-    pub fn set_context_entries(&mut self, e: u8) -> &mut Self {
-        self.0[0].set_bits(27..=31, e.into());
-        self
-    }
-
-    /// Sets the value of the Root Hub Port Number field.
-    pub fn set_root_hub_port_number(&mut self, n: u8) -> &mut Self {
-        self.0[0].set_bits(16..=23, n.into());
-        self
-    }
+                /// Sets the value of the Root Hub Port Number field.
+                pub fn set_root_hub_port_number(&mut self,n:u8)->&mut Self{
+                    self.0[0].set_bits(16..=23,n.into());
+                    self
+                }
+            }
+            impl From<[u32;$len]> for [<Byte $bytes>]{
+                fn from(raw:[u32;$len])->Self{
+                    Self(raw)
+                }
+            }
+            impl AsRef<[u32]> for [<Byte $bytes>]{
+                fn as_ref(&self)->&[u32]{
+                    &self.0
+                }
+            }
+            impl AsMut<[u32]> for [<Byte $bytes>]{
+                fn as_mut(&mut self)->&mut [u32]{
+                    &mut self.0
+                }
+            }
+        }
+    };
 }
-impl From<[u32; 8]> for Slot {
-    fn from(raw: [u32; 8]) -> Self {
-        Self(raw)
-    }
-}
-impl From<[u32; 16]> for Slot {
-    fn from(raw: [u32; 16]) -> Self {
-        Self([
-            raw[0], raw[1], raw[2], raw[3], raw[4], raw[5], raw[6], raw[7],
-        ])
-    }
-}
+
+cx!(32, 8);
+cx!(64, 16);

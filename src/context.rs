4e26aeb86cc876170664a3c590fb0a4c7eb8560d
toku-sa-n <tokusan441@gmail.com>
2021-02-04 23:18:39 +0900

    docs: add for the `context` module

--- src/context.rs
+++ src/context.rs
@@ -3,6 +3,23 @@
 //! The xHC supports either 32-byte or 64-byte Contexts. You must check the Context Size bit of the
 //! HCCPARAMS1 register. If the bit is 0, use the [`byte32`] module. If the bit is 1, use the [`byte64`]
 //! module.
+//!
+//! # Examples
+//!
+//! ```no_run
+//! use xhci::{context, context::InputHandler};
+//!
+//! let mut input = context::byte32::Input::new();
+//! let input_control = input.control_mut();
+//! input_control.set_aflag(0);
+//! input_control.set_aflag(1);
+//!
+//! # let port_id = 3;
+//! let device = input.device_mut();
+//! let slot = device.slot_mut();
+//! slot.set_context_entries(1);
+//! slot.set_root_hub_port_number(port_id);
+//! ```
 
 use bit_field::BitField;
 use core::convert::TryInto;

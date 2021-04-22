bd09eb4cf9d4b3187b64e9eb97caa6aa44611527
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-22 12:55:19 +0900

    feat: add `supported_protocol::Header`

--- src/extended_capabilities/supported_protocol.rs
+++ src/extended_capabilities/supported_protocol.rs
@@ -5,6 +5,104 @@ use core::convert::TryInto;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// The first 16 bytes of xHCI Supported Protocol Capability.
+#[repr(transparent)]
+#[derive(Copy, Clone)]
+pub struct Header([u32; 4]);
+impl Header {
+    /// Returns the value of the Minor Revision field.
+    pub fn minor_revision(self) -> u8 {
+        self.0[0].get_bits(16..=23).try_into().unwrap()
+    }
+
+    /// Returns the value of the Major Revision field.
+    pub fn major_revision(self) -> u8 {
+        self.0[0].get_bits(24..=31).try_into().unwrap()
+    }
+
+    /// Returns the value of the Name String field.
+    pub fn name_string(&self) -> u32 {
+        self.0[1]
+    }
+
+    /// Returns the value of the Compatible Port Offset field.
+    pub fn compatible_port_offset(self) -> u8 {
+        self.0[2].get_bits(0..=7).try_into().unwrap()
+    }
+
+    /// Returns the value of the Compatible Port Count field.
+    pub fn compatible_port_count(self) -> u8 {
+        self.0[2].get_bits(8..=15).try_into().unwrap()
+    }
+
+    /// Returns the Link Soft Error Count Capability bit.
+    ///
+    /// **This bit is only valid for USB3.**
+    pub fn link_soft_error_count_capability(self) -> bool {
+        self.0[2].get_bit(24)
+    }
+
+    /// Returns the High-speed Only bit.
+    ///
+    /// **This bit is only valid for USB2.**
+    pub fn high_speed_only(self) -> bool {
+        self.0[2].get_bit(17)
+    }
+
+    /// Returns the Integrated Hub Implemented bit.
+    ///
+    /// **This bit is only valid for USB2.**
+    pub fn integrated_hub_implemented(self) -> bool {
+        self.0[2].get_bit(18)
+    }
+
+    /// Returns the Hardware LPM Capability bit.
+    ///
+    /// **This bit is only valid for USB2.**
+    pub fn hardware_lpm_capability(self) -> bool {
+        self.0[2].get_bit(19)
+    }
+
+    /// Returns the BESL LPM Capability bit.
+    ///
+    /// **This bit is only valid for USB2.**
+    pub fn besl_lpm_capability(self) -> bool {
+        self.0[2].get_bit(20)
+    }
+
+    /// Returns the value of the Hub Depth field.
+    pub fn hub_depth(self) -> u8 {
+        self.0[2].get_bits(25..=27).try_into().unwrap()
+    }
+
+    /// Returns the value of the Protocol Speed ID Count field.
+    pub fn protocol_speed_id_count(self) -> u8 {
+        self.0[2].get_bits(28..=31).try_into().unwrap()
+    }
+
+    /// Returns the value of the Protocol Slot Type field.
+    pub fn protocol_slot_type(self) -> u8 {
+        self.0[3].get_bits(0..=4).try_into().unwrap()
+    }
+}
+impl_debug_from_methods! {
+    Header {
+        minor_revision,
+        major_revision,
+        name_string,
+        compatible_port_offset,
+        compatible_port_count,
+        link_soft_error_count_capability,
+        high_speed_only,
+        integrated_hub_implemented,
+        hardware_lpm_capability,
+        besl_lpm_capability,
+        hub_depth,
+        protocol_speed_id_count,
+        protocol_slot_type,
+    }
+}
+
 /// Protocol Speed ID
 #[repr(transparent)]
 #[derive(Copy, Clone)]

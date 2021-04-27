6d19541e349d23df60f7447e8445f496ddbbbc6e
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-27 11:19:51 +0900

    feat: define `IdRegister`

--- src/extended_capabilities/debug.rs
+++ src/extended_capabilities/debug.rs
@@ -1,1 +1,20 @@
 //! Debug Capability.
+
+use bit_field::BitField;
+use core::convert::TryInto;
+
+/// Debug Capability ID Register.
+#[repr(transparent)]
+#[derive(Copy, Clone)]
+pub struct IdRegister(u32);
+impl IdRegister {
+    /// Returns the value of the Debug Capability Event Ring Segment Table Max field.
+    pub fn debug_capability_event_ring_segment_table_max(self) -> u8 {
+        self.0.get_bits(16..=20).try_into().unwrap()
+    }
+}
+impl_debug_from_methods! {
+    IdRegister {
+        debug_capability_event_ring_segment_table_max,
+    }
+}

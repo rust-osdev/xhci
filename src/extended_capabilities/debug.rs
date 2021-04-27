1010da90df704fcc3a0f9e2af857b2e0402c7236
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-27 11:46:53 +0900

    feat: define `EventRingDequeuePointer`

--- src/extended_capabilities/debug.rs
+++ src/extended_capabilities/debug.rs
@@ -83,3 +83,46 @@ impl_debug_from_methods! {
         get,
     }
 }
+
+/// Debug Capability Event Ring Dequeue Pointer Register.
+#[repr(transparent)]
+#[derive(Copy, Clone)]
+pub struct EventRingDequeuePointer(u64);
+impl EventRingDequeuePointer {
+    /// Returns the value of the Dequeue ERST Segment Index field.
+    #[must_use]
+    pub fn dequeue_erst_segment_index(self) -> u8 {
+        self.0.get_bits(0..=2).try_into().unwrap()
+    }
+
+    /// Sets the value of the Dequeue ERST Segment Index field.
+    pub fn set_dequeue_erst_segment_index(&mut self, i: u8) {
+        self.0.set_bits(0..=2, i.into());
+    }
+
+    /// Returns the value of the Dequeue Pointer field.
+    #[must_use]
+    pub fn dequeue_pointer(self) -> u64 {
+        self.0 & !0b1111
+    }
+
+    /// Sets the value of the Dequeue Pointer field.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if the address is not 16-byte aligned.
+    pub fn set_dequeue_pointer(&mut self, a: u64) {
+        assert!(
+            a.trailing_zeros() >= 4,
+            "The Event Ring Dequeue Pointer must be 16-byte aligned."
+        );
+
+        self.0.set_bits(4..=63, a.get_bits(4..=63));
+    }
+}
+impl_debug_from_methods! {
+    EventRingDequeuePointer {
+        dequeue_erst_segment_index,
+        dequeue_pointer,
+    }
+}

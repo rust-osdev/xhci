9296dfcc8e19075e22d697c7830c4237d6be6e77
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-29 23:44:45 +0900

    refactor: remove unused lines

--- src/context/macros.rs
+++ src/context/macros.rs
@@ -9,16 +9,6 @@ macro_rules! bit_getter_cx {
             self.as_ref()[$offset].get_bit($bit)
         }
     };
-    ($bit:literal,$method:ident,$name:literal) => {
-        #[doc = "Returns the"]
-        #[doc = $name]
-        #[doc = "bit."]
-        #[must_use]
-        fn $method(&self) -> bool {
-            use bit_field::BitField;
-            self.as_ref().get_bit($bit)
-        }
-    };
 }
 
 macro_rules! bit_modifier_cx {
@@ -41,116 +31,12 @@ macro_rules! bit_modifier_cx {
             }
         }
     };
-    ($bit:literal,$method:ident,$name:literal) => {
-        paste::paste! {
-            #[doc = "Sets the"]
-            #[doc = $name]
-            #[doc = "bit."]
-             fn [<set_ $method>](&mut self){
-                use bit_field::BitField;
-                self.as_mut().set_bit($bit,true);
-            }
-
-            #[doc = "Clears the"]
-            #[doc = $name]
-            #[doc = "bit."]
-             fn [<clear_ $method>](&mut self){
-                use bit_field::BitField;
-                self.as_mut().set_bit($bit,false);
-            }
-        }
-    };
-}
-
-macro_rules! ro_bit_cx {
-    ([$offset:literal]($bit:literal),$method:ident,$name:literal) => {
-        bit_getter_cx!([$offset]($bit), $method, $name);
-    };
-    ($bit:literal,$method:ident,$name:literal) => {
-        bit_getter_cx!($bit, $method, $name);
-    };
-}
-
-macro_rules! wo_bit_cx {
-    ([$offset:literal]($bit:literal),$method:ident,$name:literal) => {
-        bit_modifier_cx!([$offset]($bit), $method, $name);
-    };
-    ($bit:literal,$method:ident,$name:literal) => {
-        bit_modifier_cx!($bit, $method, $name);
-    };
 }
 
 macro_rules! rw_bit_cx {
     ([$offset:literal]($bit:literal),$method:ident,$name:literal) => {
         bit_getter_cx!([$offset]($bit), $method, $name);
         bit_modifier_cx!([$offset]($bit), $method, $name);
-    };
-    ($bit:literal,$method:ident,$name:literal) => {
-        bit_getter_cx!($bit, $method, $name);
-        bit_modifier_cx!($bit, $method, $name);
-    };
-}
-
-macro_rules! rw1c_bit_cx {
-    ([$offset:literal]($bit:literal),$method:ident,$name:literal) => {
-        bit_getter_cx!([$offset]($bit), $method, $name);
-        paste::paste! {
-            #[doc = "Clears the"]
-            #[doc = $name]
-            #[doc = "bit."]
-             fn [<clear_ $method>](&mut self){
-                use bit_field::BitField;
-                self.as_mut()[$offset].set_bit($bit,true);
-            }
-        }
-    };
-    ($bit:literal,$method:ident,$name:literal) => {
-        bit_getter_cx!($bit, $method, $name);
-        paste::paste! {
-            #[doc = "Clears the"]
-            #[doc = $name]
-            #[doc = "bit."]
-             fn [<clear_ $method>](&mut self){
-                use bit_field::BitField;
-                self.as_mut().set_bit($bit,true);
-            }
-        }
-    };
-}
-
-macro_rules! w1s_bit_cx {
-    ([$offset:literal]($bit:literal),$method:ident,$name:literal) => {
-        paste::paste! {
-            #[doc = "Sets the"]
-            #[doc = $name]
-            #[doc = "bit."]
-             fn [<set_ $method>](&mut self){
-                use bit_field::BitField;
-                self.as_mut()[$offset].set_bit($bit,true);
-            }
-        }
-    };
-    ($bit:literal,$method:ident,$name:literal) => {
-        paste::paste! {
-            #[doc = "Sets the"]
-            #[doc = $name]
-            #[doc = "bit."]
-             fn [<set_ $method>](&mut self){
-                use bit_field::BitField;
-                self.as_mut().set_bit($bit,true);
-            }
-        }
-    };
-}
-
-macro_rules! rw1s_bit_cx {
-    ([$offset:literal]($bit:literal),$method:ident,$name:literal) => {
-        bit_getter_cx!([$offset]($bit), $method, $name);
-        w1s_bit_cx!([$offset]($bit), $method, $name);
-    };
-    ($bit:literal,$method:ident,$name:literal) => {
-        bit_getter_cx!($bit, $method, $name);
-        w1s_bit_cx!($bit, $method, $name);
     };
 }
 
@@ -164,17 +50,6 @@ macro_rules! field_getter_cx {
             use bit_field::BitField;
             use core::convert::TryInto;
             self.as_ref()[$offset].get_bits($range).try_into().unwrap()
-        }
-    };
-    ($range:expr,$method:ident,$name:literal,$ty:ty) => {
-        #[doc = "Returns the value of the"]
-        #[doc = $name]
-        #[doc = "field."]
-        #[must_use]
-        fn $method(&self) -> $ty {
-            use bit_field::BitField;
-            use core::convert::TryInto;
-            self.as_ref().get_bits($range).try_into().unwrap()
         }
     };
 }
@@ -192,36 +67,11 @@ macro_rules! field_setter_cx {
             }
         }
     };
-    ($range:expr,$method:ident,$name:literal,$ty:ty) => {
-        paste::paste! {
-            #[doc = "Sets the value of the"]
-            #[doc = $name]
-            #[doc = "field."]
-             fn [<set_ $method>](&mut self,value:$ty){
-                use bit_field::BitField;
-                use core::convert::TryInto;
-                self.as_mut().set_bits($range,value.try_into().unwrap());
-            }
-        }
-    };
-}
-
-macro_rules! ro_field_cx {
-    ([$offset:literal]($range:expr),$method:ident,$name:literal,$ty:ty) => {
-        field_getter_cx!([$offset]($range), $method, $name, $ty);
-    };
-    ($range:expr,$method:ident,$name:literal,$ty:ty) => {
-        field_getter_cx!($range, $method, $name, $ty);
-    };
 }
 
 macro_rules! rw_field_cx {
     ([$offset:literal]($range:expr),$method:ident,$name:literal,$ty:ty) => {
         field_getter_cx!([$offset]($range), $method, $name, $ty);
         field_setter_cx!([$offset]($range), $method, $name, $ty);
-    };
-    ($range:expr,$method:ident,$name:literal,$ty:ty) => {
-        field_getter_cx!($range, $method, $name, $ty);
-        field_setter_cx!($range, $method, $name, $ty);
     };
 }

7efbe4bf03a2302b251b90b209567d19be6cee09
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-29 18:26:24 +0900

    feat(context::Endpoint): implement accessors

--- src/context.rs
+++ src/context.rs
@@ -1,5 +1,7 @@
 //! The xHC Contexts.
 
+use core::convert::TryInto;
+
 use bit_field::BitField;
 
 macro_rules! impl_constructor {
@@ -218,6 +220,60 @@ impl<const N: usize> Slot<N> {
 pub struct Endpoint<const N: usize>([u32; N]);
 impl_constructor!(Endpoint, "Endpoint");
 impl<const N: usize> Endpoint<N> {
+    // TODO: Define `EndpointState` enum.
+    rw_field!([0](0..=2), endpoint_state, "Endpoint State", u8);
+    rw_field!([0](8..=9), mult, "Mult", u8);
+    rw_field!([0](10..=14), max_primary_streams, "Max Primary Streams", u8);
+    rw_bit!([0](15), linear_stream_array, "Linear Stream Array");
+    rw_field!([0](16..=23), interval, "Interval", u8);
+    rw_field!(
+        [0](24..=31),
+        max_endpoint_service_time_interval_payload_high,
+        "Max Endpoint Service Time Interval Payload High",
+        u8
+    );
+
+    rw_field!([1](1..=2), error_count, "Error Count", u8);
+    // TODO: Define `EndpointType` enum.
+    rw_field!([1](3..=5), endpoint_type, "Endpoint Type", u8);
+    rw_bit!([1](7), host_initiate_disable, "Host Initiate Disable");
+    rw_field!([1](8..=15), max_burst_size, "Max Burst Size", u8);
+    rw_field!([1](16..=31), max_packet_size, "Max Packet Size", u16);
+
+    rw_bit!([2](0), dequeue_cycle_state, "Dequeue Cycle State");
+
+    /// Returns the TR Dequeue Pointer.
+    pub fn tr_dequeue_pointer(self) -> u64 {
+        let l: u64 = self.0[2].into();
+        let u: u64 = self.0[3].into();
+
+        (u << 32) | l
+    }
+
+    /// Sets the TR Dequeue Pointer.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if `addr` is not 64-byte aligned.
+    pub fn set_tr_dequeue_pointer(&mut self, a: u64) -> &mut Self {
+        assert_eq!(a % 64, 0, "TR Dequeue Pointer must be 64-byte aligned.");
+
+        let l: u32 = a.get_bits(0..32).try_into().unwrap();
+        let u: u32 = a.get_bits(32..64).try_into().unwrap();
+
+        self.0[2] = l;
+        self.0[3] = u;
+        self
+    }
+
+    rw_field!([4](0..=15), average_trb_length, "Average TRB Length", u16);
+    rw_field!(
+        [4](16..=31),
+        max_endpoint_service_time_interval_payload_low,
+        "Max Endpoint Service Time Interval Payload Low",
+        u16
+    );
+
     const fn new() -> Self {
         Self([0; N])
     }

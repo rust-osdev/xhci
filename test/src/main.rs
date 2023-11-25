f12c6817362e3a53cea1ee8a78a1d5e57740aa61
Hiroki Tokunaga <tokusan441@gmail.com>
2023-11-25 13:21:55 +0900

    Build an UEFI image

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use qemu_exit::QEMUExit;
+
+#[uefi::entry]
+fn main(image: uefi::Handle, st: uefi::table::SystemTable<uefi::table::Boot>) -> uefi::Status {
+    let handler = qemu_exit::X86::new(0xf4, 33);
+
+    handler.exit_success();
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let handler = qemu_exit::X86::new(0xf4, 33);
+
+    handler.exit_failure();
 }

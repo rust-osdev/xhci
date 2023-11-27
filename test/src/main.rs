d2c72e0283d9ad262c373ffcdd2ebac334442a7f
Hiroki Tokunaga <tokusan441@gmail.com>
2023-11-27 12:15:39 +0900

    Printing

--- test/src/main.rs
+++ test/src/main.rs
@@ -2,17 +2,26 @@
 #![no_main]
 
 use qemu_exit::QEMUExit;
+use qemu_print::qemu_println;
+use uefi::table::boot::MemoryType;
 
 #[uefi::entry]
 fn main(image: uefi::Handle, st: uefi::table::SystemTable<uefi::table::Boot>) -> uefi::Status {
-    let handler = qemu_exit::X86::new(0xf4, 33);
+    let (_, memory_map) = st.exit_boot_services(MemoryType::LOADER_DATA);
 
+    for descriptor in memory_map.entries() {
+        qemu_println!("{:?}", descriptor);
+    }
+
+    let handler = qemu_exit::X86::new(0xf4, 33);
     handler.exit_success();
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     let handler = qemu_exit::X86::new(0xf4, 33);
+
+    qemu_println!("panic: {:?}", _info);
 
     handler.exit_failure();
 }

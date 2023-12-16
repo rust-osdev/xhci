d3ec6ce7b2f0f6f4cbab8d5cb8437012e8e68a0a
Hiroki Tokunaga <tokusan441@gmail.com>
2023-12-17 01:02:28 +0900

    Comment

--- test/src/registers.rs
+++ test/src/registers.rs
@@ -5,14 +5,12 @@ pub type Registers = xhci::Registers<Mapper>;
 /// # Safety
 ///
 /// Multiple returned values must not exist in the same scope.
-//
-// See [1] or [2] to understand this process.
-//
-// [1]: PCI - OSDev Wiki (https://wiki.osdev.org/PCI#Configuration_Space_Access_Mechanism_.231)
-// [2]: USB 3.0 ホストドライバ自作入門 第2章 (https://booth.pm/ja/items/1056355)
 pub unsafe fn get_accessor() -> Registers {
     let xhc_config_space = crate::pci::iter_xhc().next().expect("xHC not found");
 
+    // See [1] for the structure of base address registers.
+    //
+    // [1]: https://wiki.osdev.org/PCI#Base_Address_Registers
     let mmio_low = xhc_config_space.base_address_register(0);
     let mmio_high = xhc_config_space.base_address_register(1);
 

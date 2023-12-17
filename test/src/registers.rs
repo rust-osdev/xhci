6b195bc0f5d3e4557abf1d3c139c3d0f484031ca
Hiroki Tokunaga <tokusan441@gmail.com>
2023-12-17 11:22:07 +0900

    Docs

--- test/src/registers.rs
+++ test/src/registers.rs
@@ -1,3 +1,5 @@
+use bit_field::BitField;
+
 use crate::mapper::Mapper;
 
 pub type Registers = xhci::Registers<Mapper>;
@@ -13,6 +15,13 @@ pub unsafe fn get_accessor() -> Registers {
     // [1]: https://wiki.osdev.org/PCI#Base_Address_Registers
     let mmio_low = xhc_config_space.base_address_register(0);
     let mmio_high = xhc_config_space.base_address_register(1);
+
+    let bar_type = mmio_low.get_bits(1..=2);
+
+    assert_eq!(
+        bar_type, 2,
+        "The MMIO of xHC must be mapped to memory and 64-bit wide."
+    );
 
     let mmio_base = (((mmio_high as u64) << 32) | (mmio_low as u64 & 0xffff_fff0)) as usize;
 
